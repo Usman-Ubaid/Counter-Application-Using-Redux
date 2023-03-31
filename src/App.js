28de405be77cd1134b034b24e19ebe888fdb0436
Welcome ! <usmanubaid9@gmail.com>
2023-03-31 06:13:10 +0500

    Styling

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import "./App.css";
 import { increment, decrement, addvalue } from "./actions/index";
 import { useSelector, useDispatch } from "react-redux";
 import { useState } from "react";
@@ -9,26 +10,28 @@ const App = () => {
   const incrementValue = Number(incrementAmount);
 
   return (
-    <div>
-      <h1>Counter Application</h1>
+    <div className="container">
       <div>
-        <button onClick={() => dispatch(decrement)}>-</button>
-        <h1>{myState}</h1>
-        <button onClick={() => dispatch(increment)}>+</button>
-      </div>
-      <div>
-        <input
-          value={incrementAmount}
-          onChange={(e) => setIncrementAmount(e.target.value)}
-        />
+        <h1>Counter Application</h1>
+        <div>
+          <button onClick={() => dispatch(decrement)}>-</button>
+          <h1>{myState}</h1>
+          <button onClick={() => dispatch(increment)}>+</button>
+        </div>
+        <div>
+          <input
+            value={incrementAmount}
+            onChange={(e) => setIncrementAmount(e.target.value)}
+          />
 
-        <button onClick={() => dispatch(addvalue(incrementValue))}>
-          Add Value
-        </button>
-      </div>
-      <div>
-        <h3>Adding Asynchronously</h3>
-        <input />
+          <button onClick={() => dispatch(addvalue(incrementValue))}>
+            Add Value
+          </button>
+        </div>
+        <div>
+          <h3>Adding Asynchronously</h3>
+          <input />
+        </div>
       </div>
     </div>
   );
